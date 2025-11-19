b8790a5d10d763d264a51c4d370d330ced5a78f4
JarnKet <ketsadaphoneOfficial@gmail.com>
2025-11-19 13:39:23 +0700

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-web-101/",
+  // base: "/react-web-101/",
+  base: "https://jarnket.github.io/react-web-101/",
   plugins: [
     react({
       babel: {
