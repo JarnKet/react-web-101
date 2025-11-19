39f84dd9866e65f22459a86b783202ed22e4a2e3
JarnKet <ketsadaphoneOfficial@gmail.com>
2025-11-19 13:46:20 +0700

    set router basename

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // base: "/react-web-101/",
-  base: "https://jarnket.github.io/react-web-101/",
+  base: "/react-web-101/",
   plugins: [
     react({
       babel: {
